d2fa4c9bfccfedfe8d10c68e704b7afe670b02d9
Evan Irla <ageekstouch@gmail.com>
2021-01-02 15:39:50 -0700

    Fixing service collection extensions

--- Snacks.Entity.Authorization/Extensions/ServiceCollectionExtensions.cs
+++ Snacks.Entity.Authorization/Extensions/ServiceCollectionExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.DependencyInjection;
+using Snacks.Entity.Authorization.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Snacks.Entity.Authorization.Extensions
@@ -13,24 +15,56 @@ namespace Snacks.Entity.Authorization.Extensions
             AppDomain.CurrentDomain.GetAssemblies()
                 .Where(x => !x.FullName.Contains("Snacks.Entity.Authorization"))
                 .SelectMany(x => x.GetTypes())
-                .Where(x => typeof(BaseEntitySecurityHandler<,>).IsAssignableFrom(x))
-                .Where(x => !x.IsAbstract && !x.IsInterface);
+                .Where(x => !x.IsAbstract && !x.IsInterface)
+                .Where(x => x.IsAssignableToGenericType(typeof(BaseEntitySecurityHandler<,>)));
 
         static IEnumerable<Type> EntitySecurityRequirements =>
             AppDomain.CurrentDomain.GetAssemblies()
                 .Where(x => !x.FullName.Contains("Snacks.Entity.Authorization"))
                 .SelectMany(x => x.GetTypes())
-                .Where(x => typeof(IEntitySecurityRequirement<>).IsAssignableFrom(x))
-                .Where(x => !x.IsAbstract && !x.IsInterface);
+                .Where(x => !x.IsAbstract && !x.IsInterface)
+                .Where(x => x.IsAssignableToGenericType(typeof(IEntitySecurityRequirement<>)));
 
-        public static IServiceCollection AddEntitySecurity(this IServiceCollection services)
+        public static IServiceCollection AddEntityAuthorization(this IServiceCollection services, Action<AuthorizationOptions> configure = null)
         {
+            services.AddAuthorizationCore(options =>
+            {
+                foreach (Type requirement in EntitySecurityRequirements)
+                {
+                    PolicyAttribute policyAttribute = requirement.GetCustomAttribute<PolicyAttribute>();
+
+                    string policyName = policyAttribute?.Name ?? (GetModelType(requirement).Name + "Security");
+
+                    options.AddPolicy(policyName, policy =>
+                    {
+                        policy.Requirements.Add((IAuthorizationRequirement)Activator.CreateInstance(requirement));
+                    });
+                }
+
+                if (configure != null)
+                {
+                    configure.Invoke(options);
+                }
+            });
+
             foreach (Type handler in EntitySecurityHandlers)
             {
-                services.AddSingleton(typeof(IAuthorizationHandler), Activator.CreateInstance(handler));
+                services.AddSingleton(typeof(IAuthorizationHandler), handler);
             }
 
             return services;
+        }
+
+        private static Type GetModelType(Type requirementType)
+        {
+            Type modelType = requirementType.BaseType.GetGenericArguments().FirstOrDefault();
+
+            if (modelType == null)
+            {
+                modelType = requirementType.GetInterface("IEntitySecurityRequirement`1").GetGenericArguments().FirstOrDefault();
+            }
+
+            return modelType;
         }
     }
 }
---
+++ Snacks.Entity.Authorization/Extensions/TypeExtensions.cs
@@ -0,0 +1,35 @@
+﻿// https://stackoverflow.com/a/1075059
+using System;
+
+namespace Snacks.Entity.Authorization.Extensions
+{
+    static class TypeExtensions
+    {
+        public static bool IsAssignableToGenericType(this Type givenType, Type genericType)
+        {
+            var interfaceTypes = givenType.GetInterfaces();
+
+            foreach (var it in interfaceTypes)
+            {
+                if (it.IsGenericType && it.GetGenericTypeDefinition() == genericType)
+                {
+                    return true;
+                }
+            }
+
+            if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+            {
+                return true;
+            }
+
+            Type baseType = givenType.BaseType;
+
+            if (baseType == null)
+            {
+                return false;
+            }
+
+            return IsAssignableToGenericType(baseType, genericType);
+        }
+    }
+}
